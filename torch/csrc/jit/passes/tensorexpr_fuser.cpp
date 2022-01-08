33feb37534d32fe8c29a49d5c883982088d90322
David Berard <dberard@fb.com>
2022-01-07 17:38:41 -0800

    [WIP][JIT] enable profiling optional tensors
    ghstack-source-id: 10678eda1173c43654435706c830ed27e451f792
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/70532

--- torch/csrc/jit/passes/tensorexpr_fuser.cpp
+++ torch/csrc/jit/passes/tensorexpr_fuser.cpp
@@ -184,6 +184,9 @@ void removeProfileNodesAndSpecializeTypes(Block* b) {
       it->output()->replaceAllUsesWith(it->input());
       auto profiled_type = it->ty(attr::profiled_type)->expect<TensorType>();
 
+      // if seen_none != 0, then a NoneType has been observed.
+      auto seen_none = it->i(attr::seen_none);
+
       // A value can be profiled with differently typed uses.
       // This can occur from:
       // - having a use which is not executed, so the type will be
@@ -200,21 +203,52 @@ void removeProfileNodesAndSpecializeTypes(Block* b) {
       // a use with a different profiled type, we will still be correct.
       // In the future we could consider unifying the types of uses, or adding a
       // type refinement node so uses can have the correct corresponding type.
+      /*
       if (profiled_type == TensorType::get()) {
         continue;
       }
-      // If we encounter non-identical profiled types for the same value, merge
-      // them.  This situation can happen if, e.g., loop unrolling duplicates
-      // profiled types in a loop body in a manner that isn't logically
-      // consistent (see TestTEFuser.test_unrolled_cat).
       auto input_type = it->input()->type()->expect<TensorType>();
       if (input_type == TensorType::get()) {
         it->input()->setType(profiled_type);
       } else {
         it->input()->setType(input_type->merge(*profiled_type));
       }
-      it.destroyCurrent();
+      */
+      if (profiled_type == TensorType::get() && !seen_none) {
+        continue;
+      }
+      // If we encounter non-identical profiled types for the same value, merge
+      // them.  This situation can happen if, e.g., loop unrolling duplicates
+      // profiled types in a loop body in a manner that isn't logically
+      // consistent (see TestTEFuser.test_unrolled_cat).
+      TensorTypePtr input_tensor_type = nullptr;
+      if (it->input()->type()->kind() == c10::TypeKind::TensorType) {
+        input_tensor_type = it->input()->type()->expect<TensorType>();
+      } else if (it->input()->type()->kind() == c10::TypeKind::OptionalType) {
+        input_tensor_type = it->input()->type()->expect<OptionalType>()->getElementType()->expect<TensorType>();
+      }
 
+      TensorTypePtr merged_tensor_type = profiled_type;
+      if (input_tensor_type != TensorType::get()) {
+        merged_tensor_type = input_tensor_type->merge(*profiled_type);
+      }
+
+      // TODO: currently, we ignore whether or not input_tensor is a Tensor or
+      // an Optional[Tensor]. The reason we do this is because the type will
+      // always start as Optional[Tensor] (if the input was an optional tensor)
+      // but we want to be able to specialize on non-optional Tensor if profiling
+      // shows that the input is never None. However, this might be problematic
+      // if on one profiling run we see that it is optional, and on the second
+      // run we don't see any `None` tensors.
+      // We could try adding a `seen_none` attribute somewhere... not really sure
+      // how to do do this though
+      if (seen_none) {
+        it->input()->setType(OptionalType::create(merged_tensor_type));
+      } else {
+        it->input()->setType(merged_tensor_type);
+      }
+
+      it.destroyCurrent();
     } else {
       for (Block* ib : it->blocks()) {
         removeProfileNodesAndSpecializeTypes(ib);
