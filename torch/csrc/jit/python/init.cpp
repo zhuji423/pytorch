68f5fe46f3398e56d236989efc3a42c9d14e31bc
Ansley Ussery <ansley@fb.com>
2022-01-11 09:30:57 -0800

    Move helper structs/methods out of `ir_emitter.cpp`
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/70746
    
    Differential Revision: [D33431492](https://our.internmc.facebook.com/intern/diff/D33431492/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D33431492/)!
    ghstack-source-id: 146855261

--- torch/csrc/jit/python/init.cpp
+++ torch/csrc/jit/python/init.cpp
@@ -292,12 +292,12 @@ void initJITBindings(PyObject* module) {
       .def(
           "_jit_pass_dce",
           [](std::shared_ptr<Graph>& g) {
-            return EliminateDeadCode(g->block()); // overload resolution
+            return eliminateDeadCode(g->block()); // overload resolution
           })
       .def(
           "_jit_pass_dce_allow_deleting_nodes_with_side_effects",
           [](std::shared_ptr<Graph>& g) {
-            return EliminateDeadCode(
+            return eliminateDeadCode(
                 g->block(),
                 true,
                 DCESideEffectPolicy::
@@ -449,7 +449,7 @@ void initJITBindings(PyObject* module) {
           py::arg("g"),
           py::arg("value_name_pairs") =
               std::vector<std::pair<std::string, std::string>>())
-      .def("_jit_pass_constant_pooling", ConstantPooling)
+      .def("_jit_pass_constant_pooling", constantPooling)
       // RemoveInplaceOps is used by CoreML so it must be removed with care.
       .def("_jit_pass_propagate_dtype", DtypePropagation)
       .def(
@@ -500,7 +500,7 @@ void initJITBindings(PyObject* module) {
       .def(
           "_jit_pass_canonicalize",
           [](const std::shared_ptr<Graph>& g) { return Canonicalize(g); })
-      .def("_jit_pass_lint", LintGraph)
+      .def("_jit_pass_lint", lintGraph)
       .def(
           "_jit_pass_complete_shape_analysis",
           [](const std::shared_ptr<Graph>& graph,
@@ -596,7 +596,7 @@ void initJITBindings(PyObject* module) {
       .def(
           "_jit_pass_constant_propagation_immutable_types",
           [](std::shared_ptr<Graph>& g) {
-            return ConstantPropagationImmutableTypes(g);
+            return constantPropagationImmutableTypes(g);
           })
       .def(
           "_jit_pass_constant_propagation",
